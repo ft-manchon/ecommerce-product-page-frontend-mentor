045145fc1ffff2b0a32e1dc99a987a020f8fbab9
Fernanda Tomita <ft.manchon@gmail.com>
2024-09-05 16:47:13 -0300

    fixing the display of product images

--- src/app/ui/product/images.tsx
+++ src/app/ui/product/images.tsx
@@ -4,59 +4,71 @@ import { useState } from "react";
 import Image from "next/image";
 import { IoIosArrowForward } from "react-icons/io";
 
-
-const productImages = [
-  '/images/image-product-1',
-  '/images/image-product-2',
-  '/images/image-product-3',
-  '/images/image-product-4'
-]
 export default function Images() {
-  const [image, setImage] = useState(0);
+  const [image, setImage] = useState("/images/image-product-1");
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-  const handleClick = () => {
-    if (image <= productImages.length) {
-      setImage(image + 1);
-    } else {
-      setImage(0)
-    }
-    console.log(productImages[image]);
+  const productImages = [
+    '/images/image-product-1',
+    '/images/image-product-2',
+    '/images/image-product-3',
+    '/images/image-product-4'
+  ]
+
+  const previousImage = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === 0 ? productImages.length - 1 : prevIndex - 1));
+  }
+
+  const nextImage = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === productImages.length - 1 ? 0 : prevIndex + 1));
   }
 
   return (
     <section className="sm:max-w-[27rem] lg:min-w-[27rem] sm:py-8">
       <div className="relative w-full h-1/4">
-        <div className="absolute top-1/2 left-4 bg-white rounded-full w-9 h-9 flex items-center justify-center sm:hidden">
+        <button className="absolute top-1/2 left-4 bg-white rounded-full w-9 h-9 flex items-center justify-center sm:hidden" onClick={previousImage}>
           <IoIosArrowForward className="text-xl rotate-180 pl-[0.1rem]" />
-        </div>
+        </button>
         <Image
-          src={productImages[image] + '.jpg'}
+          src={productImages[currentIndex] + '.jpg'}
           alt="Product image"
           width={500}
-          height={500}
-          className="w-full sm:rounded-xl"
+          height={250}
+          className="w-full sm:rounded-xl sm:hidden"
           id="img-big"
           priority
         ></Image>
-        <div className="absolute top-1/2 right-4 bg-white rounded-full w-9 h-9 flex items-center justify-center sm:hidden">
+        <Image
+          src={image + '.jpg'}
+          alt="Product image"
+          width={500}
+          height={500}
+          className="hidden sm:block w-full sm:rounded-xl"
+          id="img-big"
+          priority
+        ></Image>
+        <button className="absolute top-1/2 right-4 bg-white rounded-full w-9 h-9 flex items-center justify-center sm:hidden" onClick={nextImage}>
           <IoIosArrowForward className="text-xl pl-[0.1rem]" />
-        </div>
+        </button>
       </div>
       <div className="flex items-center justify-between pt-7">
-        {productImages.map((image) => {
+
+        {productImages.map((image, index) => {
           let src = image + '-thumbnail.jpg';
           return (
             <Image
               src={src}
-              alt="Product image"
+              key={index}
+              alt={"Product thumbnail " + (index + 1)}
               width={90}
               height={90}
               className="hidden sm:block rounded-xl hover:opacity-30 hover:cursor-pointer border-2 border-transparent "
               id={image}
-              onClick={handleClick}
+              onClick={() => setImage(image)}
             ></Image>
           )
         })}
+
       </div>
     </section>
   )
